cac3063f815cb6ae9dbb330df041d0273efca507
RodBarenco <rodrigobarenco@hotmail.com>
2023-05-03 16:52:03 -0300

    just checking 10

--- src/routes.js
+++ src/routes.js
@@ -6,7 +6,7 @@ const router = Router();
 router.get('/', (req, res) => {
   fs.readFile('./src/template.html', (err, data) => {
     if (err) {
-      res.status(500).send(`Erro interno do servidor + diretório e processo: \n ${console.log(__dirname, process)}`);
+      res.status(500).send(`Erro interno do servidor + diretório e processo: \n ${console.log(__dirname)}`);
     } else {
       res.status(200).type('html').send(data);
     }
