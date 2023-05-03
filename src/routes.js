b2a6944f34245556e6f10a72a6c2ddbe8e68042e
RodBarenco <rodrigobarenco@hotmail.com>
2023-05-03 19:21:18 -0300

    try with path

--- src/routes.js
+++ src/routes.js
@@ -1,59 +1,59 @@
 import { Router } from 'express';
 import fs from 'fs';
+import path from 'path';
 
 const router = Router();
 
 router.get('/', (req, res) => {
-  fs.readFile('./src/template.html', (err, data) => {
-    if (err) {
-      res.status(500).send('Erro interno do servidor');
-    } else {
-      res.status(200).type('html').send(data);
-    }
-  });
+  const file = path.join(process.cwd(), 'src', 'template.html');
+  try {
+    const data = fs.readFileSync(file, 'utf8');
+    res.status(200).type('html').send(data);
+  } catch (err) {
+    res.status(500).send('Erro interno do servidor');
+  }
 });
 
 router.get('/list.json', (req, res) => {
-  fs.readFile('./src/data/list.json', (err, data) => {
-    if (err) {
-      res.status(500).send('Erro interno do servidor');
-    } else {
-      res.status(200).type('json').send(data);
-    }
-  });
+  const file = path.join(process.cwd(), 'src', 'data', 'list.json');
+  try {
+    const data = fs.readFileSync(file, 'utf8');
+    res.status(200).type('json').send(data);
+  } catch (err) {
+    res.status(500).send('Erro interno do servidor');
+  }
 });
 
 router.get('/main.js', (req, res) => {
-  fs.readFile('./src/main.js', (err, data) => {
-    if (err) {
-      res.status(500).send('Erro interno do servidor');
-    } else {
-      res.status(200).type('js').send(data);
-    }
-  });
+  const file = path.join(process.cwd(), 'src', 'main.js');
+  try {
+    const data = fs.readFileSync(file, 'utf8');
+    res.status(200).type('js').send(data);
+  } catch (err) {
+    res.status(500).send('Erro interno do servidor');
+  }
 });
 
 router.get('/stylesheet.css', (req, res) => {
-  fs.readFile('./src/stylesheet.css', (err, data) => {
-    if (err) {
-      res.status(500).send('Erro interno do servidor');
-    } else {
-      res.status(200).type('css').send(data);
-    }
-  });
+  const file = path.join(process.cwd(), 'src', 'stylesheet.css');
+  try {
+    const data = fs.readFileSync(file, 'utf8');
+    res.status(200).type('css').send(data);
+  } catch (err) {
+    res.status(500).send('Erro interno do servidor');
+  }
 });
 
 router.get('/assets/:filename', (req, res) => {
   const filename = req.params.filename;
-  fs.readFile(`./src/assets/${decodeURI(filename)}`, (err, data) => {
-    if (err) {
-      res.status(500).send('Erro interno do servidor');
-    } else {
-      const extension = filename.split('.').pop();
-      res.status(200).type(extension).send(data);
-    }
-  });
+  const file = path.join(process.cwd(), 'src', 'assets', decodeURI(filename));
+  try {
+    const data = fs.readFileSync(file);
+    const extension = filename.split('.').pop();
+    res.status(200).type(extension).send(data);
+  } catch (err) {
+    res.status(500).send('Erro interno do servidor');
+  }
 });
 
 export default router;
-
